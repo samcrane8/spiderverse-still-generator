718e694ad837acdeb9d374885fc42104201a3c9a
Sam Crane <samcrane8@gmail.com>
2019-10-29 17:44:24 -0400

    Setup Docker/Static Files
    - now a random files is served as the background via nginx
    - all stays inside the docker containers
    - one line docker-compose orchestration
    - actual layout of website is for tomorrow

---
+++ webapp/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {HttpClient, HttpParams, HttpHeaderResponse, HttpHeaders} from '@angular/common/http';
+import { element } from 'protractor';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title="guy"
+  stills: any;
+  backgroundImage: any;
+  constructor(private http: HttpClient) {
+  }
+
+  async ngOnInit() {
+    await this.http.get('http://localhost/static_files/')
+    .subscribe(response => {
+      this.stills = response
+      let length = this.stills.length
+      let random_selection = Math.floor(Math.random() * Math.floor(length))
+      console.log(this.stills[random_selection].name)
+      console.log(`guy: ${random_selection}`)
+      this.backgroundImage = `http://localhost/static_files/${encodeURIComponent(this.stills[random_selection].name)}`
+        
+      }
+    );
+  }
+
+  getUrl() {
+    console.log(this.backgroundImage)
+    return this.backgroundImage
+  }
+}
